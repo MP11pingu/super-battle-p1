e2dd0be376d4049858a12990ca60148b3d6082b6
MP11pingu <110827503+MP11pingu@users.noreply.github.com>
2022-08-08 15:50:31 +0200

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,56 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myAnim0":
+            case "anim2":return [img`
+. . . . . . 8 8 8 8 . . . . . . 
+. . . . 8 8 8 8 8 8 8 8 . . . . 
+. . . 8 8 8 8 8 8 8 8 8 8 . . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+. 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . . 8 8 8 8 8 8 8 8 8 8 . . . 
+. . 9 9 8 8 8 8 8 8 8 8 9 9 . . 
+. . 9 9 8 8 8 8 8 8 8 8 9 9 . . 
+. . 9 9 8 8 8 8 8 8 8 8 9 9 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`];
+            case "myAnim":
+            case "anim1":return [img`
+. . . . . . 8 8 8 8 . . . . . . 
+. . . . 8 8 8 8 8 8 8 8 . . . . 
+. . . 8 8 8 8 8 8 8 8 8 8 . . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. 8 8 8 8 f 8 8 f 8 8 8 8 8 8 . 
+. 8 8 8 8 f 8 8 f 8 8 8 8 8 8 . 
+. . 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+. . . 8 8 8 8 8 8 8 8 8 8 . . . 
+. . 9 9 8 8 8 8 8 8 8 8 9 9 . . 
+. . 9 9 8 8 8 8 8 8 8 8 9 9 . . 
+. . 9 9 8 8 8 8 8 8 8 8 9 9 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`];
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
